27280d5692d31b890cc2d5034176ff887f8d2abd
chamo112 <rika.11270@gmail.com>
2022-09-22 20:37:03 +0900

    update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Routes } from '../src/routes';
+import Routes from './routes';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
